b2c5038e760ef2d920bc8a4f2a82ef3635fc82fe
cameronmaz <88409699+cameronmaz@users.noreply.github.com>
2025-05-24 14:47:57 -0500

    Delete volleyball-play-recorder directory

--- volleyball-play-recorder/pages/index.js
+++
@@ -1,9 +0,0 @@
-import VolleyballPlayRecorder from '../components/VolleyballPlayRecorder'
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-900">
-      <VolleyballPlayRecorder />
-    </div>
-  )
-}

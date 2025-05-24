938a745c3b721f3d81df9812937c48e6a27f2486
cameronmaz <88409699+cameronmaz@users.noreply.github.com>
2025-05-24 14:38:54 -0500

    Add files via upload

---
+++ volleyball-play-recorder/pages/index.js
@@ -0,0 +1,9 @@
+import VolleyballPlayRecorder from '../components/VolleyballPlayRecorder'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <VolleyballPlayRecorder />
+    </div>
+  )
+}

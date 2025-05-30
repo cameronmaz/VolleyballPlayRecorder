e7d97c78610728b7ac2dba7ff232eeadc274acb1
cameronmaz <88409699+cameronmaz@users.noreply.github.com>
2025-05-30 18:58:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
